b92297f07760983e02f0645865a9727a43b3523b
silentss <turu@micsocks.net>
2017-06-19 03:11:47 +0300

    Update ServiceReceiver.java

--- Dendroid APK/src/com/connect/ServiceReceiver.java
+++ Dendroid APK/src/com/connect/ServiceReceiver.java
@@ -4,7 +4,7 @@ import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Date;nnn
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
